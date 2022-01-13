96cdf1b52cb184afcb90603fff32b2d2fa41ab2e
Roger Mori <roger.mori@ntierfs.com>
2022-01-12 20:23:54 -0500

    Using Stringutils and added commons-lang3

--- src/main/java/com/ntier/training/Application.java
+++ src/main/java/com/ntier/training/Application.java
@@ -2,17 +2,17 @@ package com.ntier.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     public Application() {
         System.out.println("Building instance");
     }
 
-    @Override
-    public String toString() {
-        return "application";
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words,' ');
+        return (separateWords == null) ? 0 : separateWords.length;
     }
-
     public void greet() {
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
@@ -25,5 +25,7 @@ public class Application {
         System.out.println("Application entry point");
         Application app = new Application();
         app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count : " + count);
     }
 }
